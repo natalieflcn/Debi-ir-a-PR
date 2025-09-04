124998e0e793d8b24cba06970483e567b7a90087
natalieflcn <natalie.dflcn@gmail.com>
2025-09-03 21:12:04 -0400

    Improved the data structure that held the menu category and their respective menu items in the sidebar for the Resources page. Refactored the styled components and rendered JSX within the Sidebar component to accomodate for this new data structure. Need to work on refactoring the Panel components to handle this data structure next.

--- src/features/guest/ResourcesLayout.jsx
+++ src/features/guest/ResourcesLayout.jsx
@@ -18,27 +18,60 @@ const StyledPanels = styled.div`
   --panel-color: var(--color-dark-200);
 `;
 
+// const menuItems = [
+//   { label: "Learn More About the Island", heading: "h1", link: "/resources" },
+//   { label: "Brush Up on Your Spanish", heading: "h3", link: "/resources" },
+//   {
+//     label: "Dive into Puerto Rican History",
+//     heading: "h3",
+//     link: "/resources",
+//   },
+//   { label: "Support the Island", heading: "h1", link: "/resources" },
+//   {
+//     label: "Donate to Puerto Rican Organizations",
+//     heading: "h3",
+//     link: "/resources",
+//   },
+//   { label: "Sign Petition", heading: "h3", link: "/resources" },
+//   {
+//     label: "Keep Up with Puerto Rican News",
+//     heading: "h3",
+//     link: "/resources",
+//   },
+// ];
+
 const menuItems = [
-  { label: "Learn More About the Island", heading: "h1", link: "/resources" },
-  { label: "Brush Up on Your Spanish", heading: "h3", link: "/resources" },
   {
-    label: "Dive into Puerto Rican History",
-    heading: "h3",
+    label: "Learn More About the Island",
     link: "/resources",
+    items: [
+      { label: "Brush Up on Your Spanish", link: "/resources" },
+      {
+        label: "Dive into Puerto Rican History",
+        link: "/resources",
+      },
+    ],
   },
-  { label: "Support the Island", heading: "h1", link: "/resources" },
   {
-    label: "Donate to Puerto Rican Organizations",
-    heading: "h3",
+    label: "Support the Island",
     link: "/resources",
-  },
-  { label: "Sign Petition", heading: "h3", link: "/resources" },
-  {
-    label: "Keep Up with Puerto Rican News",
-    heading: "h3",
-    link: "/resources",
+    items: [
+      {
+        label: "Donate to Puerto Rican Organizations",
+        link: "/resources",
+      },
+      { label: "Sign Petition", link: "/resources" },
+      {
+        label: "Keep Up with Puerto Rican News",
+        link: "/resources",
+      },
+    ],
   },
 ];
+
+//need to fix sidebar and how chapters + headings are rendered
+//need to fix panels and how headings, panels are rendered
+
 function ResourcesLayout() {
   return (
     <StyledResourcesLayout>
--- src/ui/elements/Sidebar.jsx
+++ src/ui/elements/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import styled, { css } from "styled-components";
+import Heading from "./Heading";
+import React from "react";
 
 const StyledSidebar = styled.aside`
   display: flex;
@@ -14,61 +16,58 @@ const StyledSidebar = styled.aside`
   box-shadow: 3px 3px 2px var(--color-red-200);
   border-radius: var(--border-radius-lg);
   list-style: none;
+  text-transform: uppercase;
+
   @media (max-width: 798px) {
     display: none;
   }
 `;
+const MenuHeading = styled(Heading)`
+  font-family: DtMF;
+  font-size: 1.75rem;
+  text-shadow: 3px 3px 1px var(--color-blue-100);
+  color: var(--color-red-200);
+  transition: all 0.2s;
+
+  &:not(:first-child) {
+    margin-top: 1rem;
+  }
+
+  &:hover {
+    color: var(--color-yellow-200);
+    text-shadow: 2px 2px 1px var(--color-red-300);
+  }
+`;
 
 const MenuItem = styled.li`
-  ${({ as }) => variants[as]}
-
-  text-transform: uppercase;
+  font-family: Museo, sans-serif;
+  font-weight: 900;
+  font-size: 1.25rem;
+  padding-left: 1.5rem;
+  border-left: 2px solid var(--color-blue-100);
+  color: var(--color-blue-200);
   transition: all 0.2s;
 
   &:hover {
     color: var(--color-yellow-200);
+    text-shadow: 2px 2px 1px var(--color-red-300);
+    border-left: 2px solid var(--color-yellow-400);
   }
 `;
-
-const variants = {
-  h1: css`
-    font-family: DtMF;
-    font-size: 1.75rem;
-
-    text-shadow: 3px 3px 1px var(--color-blue-100);
-    color: var(--color-red-200);
-
-    &:not(:first-child) {
-      margin-top: 1rem;
-    }
-
-    &:hover {
-      text-shadow: 3px 3px 1px var(--color-red-300);
-    }
-  `,
-
-  h3: css`
-    font-family: Museo, sans-serif;
-    font-weight: 900;
-    font-size: 1.25rem;
-    padding-left: 1.5rem;
-    border-left: 2px solid var(--color-blue-100);
-    color: var(--color-blue-200);
-
-    &:hover {
-      text-shadow: 2px 2px 1px var(--color-red-300);
-      border-left: 2px solid var(--color-yellow-400);
-    }
-  `,
-};
 
 function Sidebar({ menuItems }) {
   return (
     <StyledSidebar>
-      {menuItems.map((item) => (
-        <MenuItem as={item.heading} key={item.label}>
-          <NavLink to={item.link}>{item.label}</NavLink>
-        </MenuItem>
+      {menuItems.map((menuSection) => (
+        <React.Fragment key={menuSection.label}>
+          <MenuHeading>{menuSection.label}</MenuHeading>
+
+          {menuSection.items.map((item) => (
+            <MenuItem key={item.label}>
+              <NavLink to={item.link}>{item.label}</NavLink>
+            </MenuItem>
+          ))}
+        </React.Fragment>
       ))}
     </StyledSidebar>
   );
